1b28001dc7dff755f6cb717ec7cb642e688618c3
Giovanni Silva <git@gsilva.pro>
2024-10-03 21:28:51 -0300

    Initial commit

---
+++ src/main/java/br/com/eadtt/spring/p2/AulaSpringTarget2024P2Application.java
@@ -0,0 +1,13 @@
+package br.com.eadtt.spring.p2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaSpringTarget2024P2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AulaSpringTarget2024P2Application.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/eadtt/spring/p2/AulaSpringTarget2024P2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eadtt.spring.p2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaSpringTarget2024P2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
